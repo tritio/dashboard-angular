b6e9b4bcef25787ec24100dcc217e85ed938540d
tritio <tritio2@gmail.com>
2021-02-04 00:31:07 +0100

    subida carpeta componentes y modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultModule } from './layouts/default/default.module';
 import { AppComponent } from './app.component';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
+     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DefaultModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/widgets/area/area.component.ts
@@ -0,0 +1,84 @@
+import { Component, Input, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
+
+@Component({
+  selector: 'app-widget-area',
+  templateUrl: './area.component.html',
+  styleUrls: ['./area.component.scss']
+})
+export class AreaComponent implements OnInit {
+
+  chartOptions: {}
+  @Input() data: any = [];
+
+  Highcharts = Highcharts;
+  
+  constructor() { }
+
+ 
+
+  ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+          type: 'area'
+      },
+      title: {
+          text: 'Crecimiento histórico y estimado de la población mundial por regiones'
+      },
+      subtitle: {
+          text: 'Fuente: Wikipedia.org'
+      },
+      xAxis: {
+          categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+          tickmarkPlacement: 'on',
+          title: {
+              enabled: false
+          }
+      },
+      yAxis: {
+          title: {
+              text: 'Miles de millones'
+          },
+          labels: {
+              formatter: function () {
+                  return this.value / 1000;
+              }
+          }
+      },
+      tooltip: {
+          split: true,
+          valueSuffix: ' millones'
+      },
+      plotOptions: {
+          area: {
+              stacking: 'normal',
+              lineColor: '#666666',
+              lineWidth: 1,
+              marker: {
+                  lineWidth: 1,
+                  lineColor: '#666666'
+              }
+          }
+      },
+      credits: {
+        enabled: false
+      },
+      exporting: {
+        enabled: true,
+      },
+      series: this.data
+    };
+
+    HC_exporting(Highcharts);
+    
+    setTimeout(() => {
+      window.dispatchEvent(
+        new Event('resize')
+      )
+    }, 300)
+
+  }
+
+}
